7911fd31374f3ee2dc1c2e5cb0b494e0baffbb65
nishant <nishantingale17110@gmail.com>
2025-05-18 23:00:37 +0530

    pj_alphabet_pattern.java

---
+++ pj_alphabet_pattern.java
@@ -0,0 +1,98 @@
+public class pj_alphabet_pattern {
+    public static void main(String[] args) {
+
+        for (int i = 1; i<=5; i++){
+
+
+            for (int j = 1; j<=9;j++){
+
+
+                if((j == 6-i)||(i==3&&j>=3&&j<=7)||(j==4+i))
+                    System.out.print("+");
+
+                else
+                    System.out.print(" ");
+
+            }
+
+            System.out.println();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= 7; j++) {
+                if (
+                        j == 1 ||
+                                (i == 1 && j <= 6) ||
+                                (i == 4 && j <= 6) ||
+                                (i == 7 && j <= 6) ||
+                                (j == 7 && i > 1 && i < 4) ||
+                                (j == 7 && i > 4 && i < 7)
+                ) {
+                    System.out.print("+");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(); // move to next line
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
